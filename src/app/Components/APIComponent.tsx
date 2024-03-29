dc29ea68e50ba85671be5f82ed9526b6dd811676
InkoWoomy <yoda4115@gmail.com>
2024-03-29 16:36:55 -0700

    removed api

--- src/app/Components/APIComponent.tsx
+++ src/app/Components/APIComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default async function APICall(){
-    const locationPromise = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + `${currentCity},` + `${currentCountry}`+ "&appid=" + `${apiKey}`);
-    const locationData = await locationPromise.json();
-}
+// export default async function APICall(){
+//     const locationPromise = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + `${currentCity},` + `${currentCountry}`+ "&appid=" + `${apiKey}`);
+//     const locationData = await locationPromise.json();
+// }
