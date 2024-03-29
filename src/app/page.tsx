e7b74598fbe391bfffb428d2c362217c8af26132
InkoWoomy <yoda4115@gmail.com>
2024-03-29 16:39:43 -0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import Image from "next/image";
-import { Main } from "next/document";
-import dashboardTop from "./Dashboard/dashboardTop";
-import dashboardBottom from "./Dashboard/dashboardBottom";
-import TextInput from "./Components/InputComponent";
 import CityAndTemperature from "./Dashboard/dashboardTop";
 import FiveDay from "./Dashboard/dashboardBottom";
 
